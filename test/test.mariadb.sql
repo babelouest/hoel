9fc2772de15295da84966adcbf024f88d0e8f60b
Nicolas Mora <github@babelouest.org>
2022-04-29 19:43:19 -0400

    Remove backquotes

--- test/test.mariadb.sql
+++ test/test.mariadb.sql
@@ -1,10 +1,10 @@
 -- Public domain, no copyright. Use at your own risk
 
-DROP TABLE IF EXISTS `test_table`;
+DROP TABLE IF EXISTS test_table;
 
-CREATE TABLE `test_table` (
-  `id_col` INT(11) PRIMARY KEY AUTO_INCREMENT,
-  `integer_col` INT(11),
-  `string_col` VARCHAR(256),
-  `date_col` TIMESTAMP
+CREATE TABLE test_table (
+  id_col INT(11) PRIMARY KEY AUTO_INCREMENT,
+  integer_col INT(11),
+  string_col VARCHAR(256),
+  date_col TIMESTAMP
 );
--- test/test.sqlite3.sql
+++ test/test.sqlite3.sql
@@ -1,10 +1,10 @@
 -- Public domain, no copyright. Use at your own risk
 
-DROP TABLE IF EXISTS `test_table`;
+DROP TABLE IF EXISTS test_table;
 
-CREATE TABLE `test_table` (
-  `id_col` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `integer_col` INTEGER,
-  `string_col` TEXT,
-  `date_col` TIMESTAMP
+CREATE TABLE test_table (
+  id_col INTEGER PRIMARY KEY AUTOINCREMENT,
+  integer_col INTEGER,
+  string_col TEXT,
+  date_col TIMESTAMP
 );
